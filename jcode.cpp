8704b7305fa450d7cf7cb0034fd420b6583353c6
Brayden Elizier Gshnolbene <plotjointhq@gmail.com>
2015-12-08 00:12:04 -0500

    Update jcode.cpp

--- jcode.cpp
+++ jcode.cpp
@@ -1,1 +1,17 @@
+// Libraries
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+using namespace std;
 
+// Runtime Variables
+string command;
+string file_name;
+string line;
+int line_number = 0;
+bool editing_file = false;
+
+// Main Function
+int main(){
+  
+}
